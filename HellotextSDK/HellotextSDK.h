f8e155a12c2d370fac09cdf70b2ea1dbb86058a8
Breno Morais <brenomorais88@gmail.com>
2024-11-09 11:43:04 -0300

    first commit

---
+++ HellotextSDK/HellotextSDK.h
@@ -0,0 +1,18 @@
+//
+//  HellotextSDK.h
+//  HellotextSDK
+//
+//  Created by Breno Morais on 09/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HellotextSDK.
+FOUNDATION_EXPORT double HellotextSDKVersionNumber;
+
+//! Project version string for HellotextSDK.
+FOUNDATION_EXPORT const unsigned char HellotextSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HellotextSDK/PublicHeader.h>
+
+
---
+++ HellotextSDKTests/HellotextSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  HellotextSDKTests.swift
+//  HellotextSDKTests
+//
+//  Created by Breno Morais on 09/11/24.
+//
+
+import Testing
+@testable import HellotextSDK
+
+struct HellotextSDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
