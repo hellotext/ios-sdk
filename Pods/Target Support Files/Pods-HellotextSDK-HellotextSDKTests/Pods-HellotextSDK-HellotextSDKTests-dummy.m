e143ef1f31d7c07e9e10d0b0087cee9a25d80017
Breno Morais <brenomorais88@gmail.com>
2024-11-11 15:57:31 -0300

    Fix plist

---
+++ Pods/Target Support Files/Pods-HellotextSDK-HellotextSDKTests/Pods-HellotextSDK-HellotextSDKTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HellotextSDK_HellotextSDKTests : NSObject
+@end
+@implementation PodsDummy_Pods_HellotextSDK_HellotextSDKTests
+@end
---
+++ Pods/Target Support Files/Pods-HellotextSDK/Pods-HellotextSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HellotextSDK : NSObject
+@end
+@implementation PodsDummy_Pods_HellotextSDK
+@end
